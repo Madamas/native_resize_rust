eb3ec4326b18caefed2a37d55c32c2fff0cf5182
Vadym Maslov <mrvadamas@gmail.com>
2020-02-19 00:35:21 +0200

    test http server

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,56 @@
 use std::convert::Infallible;
-
-use warp::Filter;
-use serde_derive::{Deserialize};
+use std::collections::HashMap;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server, StatusCode, Method};
 use libvips::{VipsApp, ops};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
 struct ThumbOptions {
     url: String,
-    width: Option<i32>
+    width: i32
 }
 
-async fn handle_request(opts: ThumbOptions) -> Result<impl warp::Reply, Infallible> {
+impl ThumbOptions {
+    fn new(opts: HashMap<String, String>) -> ThumbOptions {
+        let url: String = match opts.get("url") {
+            Some(val) => String::from(val),
+            None => String::from("")
+        };
+
+        let width: i32 = match opts.get("width") {
+            Some(val) => val.parse::<i32>().unwrap(),
+            None => 180
+        };
+
+        ThumbOptions {
+            url: url,
+            width: width
+        }
+    }
+}
+
+fn querify(string: &str) -> HashMap<String, String> {
+    let mut acc: HashMap<String, String> = HashMap::new();
+    let pairs: Vec<&str> = string.split('&').collect();
+    for kv in pairs {
+        let mut it = kv.splitn(2, '=').take(2);
+        match (it.next(), it.next()) {
+            (Some("url"), Some(v)) => acc.insert(String::from("url"), v.to_string()),
+            (Some("width"), Some(v)) => acc.insert(String::from("width"), v.to_string()),
+            _ => continue,
+        };
+    }
+    acc
+}
+
+impl From<&str> for ThumbOptions {
+    fn from(query_params: &str) -> Self {
+        let qs = querify(query_params);
+        ThumbOptions::new(qs)
+    }
+}
+
+async fn handle_thumbnail(opts: ThumbOptions) -> Result<Vec<u8>, hyper::Error> {
     let file = reqwest::get(&opts.url)
         .await
         .expect("Async download err")
@@ -18,27 +58,56 @@ async fn handle_request(opts: ThumbOptions) -> Result<impl warp::Reply, Infallib
         .await
         .expect("Byte convert err");
 
-    let width = opts.width.unwrap_or(180);
+    let width = opts.width;
 
     let resized = ops::thumbnail_buffer(&file, width).unwrap();
-    let resized_image = resized.image_write_to_buffer(".png").unwrap();
 
-    Ok(warp::http::Response::builder()
-        .body(resized_image)
-    )
+    Ok(resized.image_write_to_buffer(".png").unwrap())
+}
+
+async fn router(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let uri = req.uri();
+    match (req.method(), uri.path()) {
+        (&Method::GET, "/thumbnail") => {
+            let q = uri.query().unwrap();
+
+            let thumb = handle_thumbnail(ThumbOptions::from(q))
+                .await?;
+
+            let response = Response::builder()
+                .status(StatusCode::OK)
+                .body(Body::from(thumb))
+                .unwrap();
+
+            Ok(response)
+        },
+        _ => {
+            let mut not_found = Response::default();
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
 }
 
 #[tokio::main]
-async fn main() {
+pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let app = VipsApp::new("Test Libvips", false).expect("Cannot initialize libvips");
     app.concurrency_set(20);
-    // POST /
-    let hello = warp::path("thumbnail")
-        .and(warp::query::<ThumbOptions>())
-        .and_then(handle_request);
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
-        .await
+    let make_svc = make_service_fn(|_conn| {
+        // This is the `Service` that will handle the connection.
+        // `service_fn` is a helper to convert a function that
+        // returns a Response into a `Service`.
+        async { Ok::<_, Infallible>(service_fn(router)) }
+    });
 
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    println!("Listening on http://{}", addr);
+
+    server.await?;
+
+    Ok(())
 }
